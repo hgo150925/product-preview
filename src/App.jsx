aeaa785796f9871a820f11d12e372a2ab2c0e1ee
hgo150925 <hugo.orlando2030@gmail.com>
2022-07-16 10:39:42 -0300

    agregando border radius al container

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 function App() {
-  return (
+  treturn(
     <section className="container">
       <div className="container__grid">
         <div className="container__text">
-          <h2>perfume</h2>
-          <h1>Gabrielle Essence Eau De Parfum</h1>
+          <p>perfume</p>
+          <h1 className="container__text--title">
+            Gabrielle Essence Eau De Parfum
+          </h1>
           <p>
             A floral, solar and voluptuous interpretation composed by Olivier
             Polge, Perfumer-Creator for the House of CHANEL.
           </p>
+          <div className="price">
+            <ul>
+              <li>$149.99</li>
+              <li>$169.99</li>
+            </ul>
+          </div>
+          <button>Add to Cart</button>
         </div>
       </div>
     </section>
