89b6419a5af799b218aabad4ad632fffe2184c45
hgo150925 <hugo.orlando2030@gmail.com>
2022-07-16 10:04:25 -0300

    agregando img

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
 function App() {
-  return <div className="App">Hola</div>;
+  return (
+    <section className="container">
+      <div className="container__grid">
+        <div className="container__text">
+          <h2>perfume</h2>
+          <h1>Gabrielle Essence Eau De Parfum</h1>
+          <p>
+            A floral, solar and voluptuous interpretation composed by Olivier
+            Polge, Perfumer-Creator for the House of CHANEL.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default App;
